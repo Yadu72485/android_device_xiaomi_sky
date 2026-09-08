a0933df1d0d2e5fc9713a08b8b55d0c7835e6c11
Yadu72485 <ppry20192021@gmail.com>
2026-09-08 11:29:18 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -416,8 +416,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/google/pixel/power-libperfmgr/libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
-    hardware/qcom-caf/sm8450/audio/agm/ipc/HwBinders/legacy \
-    hardware/xiaomi
+    hardware/qcom-caf/sm8450/audio/agm/ipc/HwBinders/legacy
 
 # Telephony
 PRODUCT_PACKAGES += \
